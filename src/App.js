7dae95324906452a390c3ef20591d5df490ca2ea
nenad_panic <nenad.panic@clariness.con>
2018-12-14 22:16:05 +0100

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import moment from 'moment';
+import Calendar from './Calendar';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: moment().startOf('year')
+    }
+  }
+
+  handlePrevYear(e) {
+    e.preventDefault();
+    this.setState({
+      date: this.state.date.clone().subtract(1, 'year')
+    });
+  }
+
+  handleNextYear(e) {
+    e.preventDefault();
+    this.setState({
+      date: this.state.date.clone().add(1, 'year')
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <a href="#" className="prevYear" onClick={(e) => this.handlePrevYear(e)}>
+          Prev Year
+        </a>
+        <a href="#" className="nextYear" onClick={(e) => this.handleNextYear(e)}>
+          Next Year
+        </a>
+        <Calendar startDate={ this.state.date }
+                  date={ this.state.date }
+                  endDate={ this.state.date.clone().endOf('year') }
+                  mods={[
+                    {
+                      date: moment(),
+                      classNames: [ 'current' ],
+                      component: [ 'day', 'month', 'week' ]
+                    },
+                    {
+                      startDate: moment().add(3, 'days'),
+                      endDate: moment().add(7, 'days'),
+                      classNames: [ 'longEvent' ],
+                      component: [ 'day' ]
+                    },
+                    {
+                      date: moment().add(3, 'days'),
+                      classNames: [ 'appointment' ],
+                      component: [ 'day' ]
+                    },
+                    {
+                      date: moment().add(4, 'days'),
+                      classNames: [ 'event', 'warning' ],
+                      component: [ 'day' ],
+                      events: {
+                        onClick: (date, e) => alert(`${date.format('dddd')}'s event!`)
+                      }
+                    },
+                    {
+                      date: moment().add(5, 'days'),
+                      classNames: [ 'event' ],
+                      component: [ 'day' ]
+                    },
+                    {
+                      component: 'day',
+                      events: {
+                        onClick: (date, e) => alert(date.format())
+                      }
+                    }
+                  ]} 
+                  />
       </div>
     );
   }
---
+++ src/Calendar.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import classnames from 'classnames';
+import { getModsByCompType } from './util';
+
+import Month from './Month';
+
+export default class Calendar extends Component {
+  moment () {
+    const localMoment = moment.apply(null, arguments);
+
+    localMoment.locale(this.props.locale);
+
+    return localMoment;
+  }
+
+  renderHeader () {
+    return (
+      <header key="header" className={classnames('rc-Calendar-header')}>
+        { this.moment(this.props.date).format('YYYY') }
+      </header>
+    );
+  }
+
+  getMonthRange () {
+    const focus = this.moment(this.props.date || this.props.startDate).startOf('month');
+    const start = this.moment(this.props.startDate);
+    const end = this.moment(this.props.endDate);
+    const size = end.diff(start, 'month') + 1;
+
+    return Array(size).fill(0).map((n, i) => focus.clone().add(n + i, 'months'));
+  }
+
+  render () {
+    const { mods } = this.props;
+    const monthMods = getModsByCompType('month', mods);
+    let weekMods = getModsByCompType('week', mods);
+    let dayMods = getModsByCompType('day', mods);
+    return (
+      <div>
+        { this.renderHeader() }
+        {
+          this.getMonthRange().map((date, i) =>
+           <Month key={ `month-${i}` }
+                  date={ date }
+                  monthNames={ true }
+                  mods={ monthMods }
+                  week={ weekMods }
+                  day={ dayMods } />
+          )
+        }
+      </div>
+    );
+  }
+}
